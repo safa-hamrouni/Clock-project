1fad8a12a460ed44ab050cb3580a0b38dc63f228
safa-hamrouni <hamrouni.safaa@gmail.com>
2020-05-22 01:35:49 +0100

    -

--- main.js
+++ main.js
@@ -1,1 +1,55 @@
-#Clock project
+
+function clock() { 
+	var i = 0;
+	var current = new Date();
+	var hour = 	current.getHours()
+	var minute = current.getMinutes()
+	var second = current.getSeconds()
+
+	return {
+		hours: function(){
+			//set live hours
+			current.getHours()
+		},
+		minutes: function(){
+			//set live minutes
+			current.getMinutes()
+		},
+		seconds: function(){
+			//set live seconds
+			current.getSeconds()
+		},
+		display: function(){
+			//display the time 
+			console.log(current.getHours())
+			$('.hours').text(hour)
+			$('.minutes').text(minute)
+			$('.seconds').text(second)
+			
+		},
+		increment: function(){
+			//increments seconds, minutes and hours
+			// i++
+			// if (second === 59){
+			// 	minute ++
+			// 	second = 0
+			// }
+			// if (minute === 59){
+			// 	hour ++ 
+			// 	minute = 0
+			// }
+			setInterval(seconds, 1000)
+		},
+		morningAlarm: function(){
+			//will ask you if you slept well and change background image (maybe have a link to news and weather today)
+
+		}
+
+	};
+}
+
+var time = clock()
+time.display()
+time.increment()
+
+
